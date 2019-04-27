0cf7153b1d053dde8fb9c13a205d7dd185699ff1
Kera <kera.andrea@gmail.com>
2019-04-27 14:53:38 -0400

    Updates

---
+++ precache-manifest.38e3ca72e0862a32634a617d9b6fa6f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3c039cc24a51dd1053527b5550dbb1",
+    "url": "/musuemapp/index.html"
+  },
+  {
+    "revision": "fbfed162b0ccfb54ea11",
+    "url": "/musuemapp/static/css/main.6f3e638b.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/musuemapp/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "fbfed162b0ccfb54ea11",
+    "url": "/musuemapp/static/js/main.6a3fcb89.chunk.js"
+  },
+  {
+    "revision": "771295280058c5545685",
+    "url": "/musuemapp/static/js/runtime~main.b91c74b6.js"
+  }
+]);
